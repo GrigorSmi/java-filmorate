c646365ea691aaddd93534f27a76a3f1eb348816
Ilya Naumenko <naumenko951294@yandex.ru>
2026-08-20 21:03:17 +0300

    Исправление Checkstyle 3

--- src/test/java/ru/yandex/practicum/filmorate/storage/db/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/db/UserDbStorageTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage.db;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
