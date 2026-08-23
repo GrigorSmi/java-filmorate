d7901d17e32a8502ec3b40a645b3924844572d89
GrigorSmi <grigor.smi.smirnov@yandex.ru>
2026-08-23 21:48:43 +0300

    Update FilmService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -115,7 +115,7 @@ public class FilmService {
                 .orElseThrow(() -> new NotFoundException("Пользователь с id=" + friendId + " не найден"));
         return filmStorage.getCommonFilms(userId, friendId);
     }
-    
+
     public List<Film> search(String query, String by) {
         return filmStorage.search(query, by);
     }
