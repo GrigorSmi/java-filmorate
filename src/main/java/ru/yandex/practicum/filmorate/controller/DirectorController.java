ca3c97957c952d642f6ee6d3f086eee1b45d3c62
Ilya Naumenko <naumenko951294@yandex.ru>
2026-08-23 18:23:39 +0300

    Функциональность Отзывы и исправление Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,15 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
-<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-=======
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
->>>>>>> develop
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.service.DirectorService;
 
@@ -18,7 +15,6 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/directors")
-<<<<<<< HEAD
 @RequiredArgsConstructor
 public class DirectorController {
     private final DirectorService directorService;
@@ -27,71 +23,62 @@ public class DirectorController {
     public Director create(@RequestBody Director director) {
         log.info("Запрос на создание режиссёра: {}", director);
         return directorService.create(director);
-=======
-public class DirectorController {
-    private final DirectorService directorService;
 
-    public DirectorController(DirectorService directorService) {
-        this.directorService = directorService;
-    }
+        public class DirectorController {
+            private final DirectorService directorService;
 
-    @GetMapping
-    public List<Director> findAll() {
-        return directorService.findAll();
-    }
+            public DirectorController(DirectorService directorService) {
+                this.directorService = directorService;
+            }
 
-    @GetMapping("/{id}")
-    public Director findById(@PathVariable Long id) {
-        return directorService.findById(id);
-    }
+            @GetMapping
+            public List<Director> findAll() {
+                return directorService.findAll();
+            }
 
-    @PostMapping
-    public Director add(@Valid @RequestBody Director director) {
-        log.info("Запрос на создание режиссёра: {}", director);
-        Director created = directorService.add(director);
-        log.info("Добавлен режиссёр: id={}, name={}", created.getId(), created.getName());
-        return created;
->>>>>>> develop
-    }
+            @GetMapping("/{id}")
+            public Director findById(@PathVariable Long id) {
+                return directorService.findById(id);
+            }
 
-    @PutMapping
-    public Director update(@Valid @RequestBody Director director) {
-        log.info("Запрос на обновление режиссёра: {}", director);
-<<<<<<< HEAD
-        return directorService.update(director);
-=======
-        if (director.getId() == null) {
-            log.warn("Ошибка: id режиссёра не указан");
-            throw new ValidationException("id режиссёра не указан");
+            @PostMapping
+            public Director add(@Valid @RequestBody Director director) {
+                log.info("Запрос на создание режиссёра: {}", director);
+                Director created = directorService.add(director);
+                log.info("Добавлен режиссёр: id={}, name={}", created.getId(), created.getName());
+                return created;
+            }
+
+            @PutMapping
+            public Director update(@Valid @RequestBody Director director) {
+                log.info("Запрос на обновление режиссёра: {}", director);
+                return directorService.update(director);
+                if (director.getId() == null) {
+                    log.warn("Ошибка: id режиссёра не указан");
+                    throw new ValidationException("id режиссёра не указан");
+                }
+                Director updated = directorService.update(director);
+                log.info("Обновлён режиссёр: id={}, name={}", updated.getId(), updated.getName());
+                return updated;
+            }
+
+            @DeleteMapping("/{id}")
+            public void delete(@PathVariable Long id) {
+                log.info("Запрос на удаление режиссёра с id={}", id);
+                directorService.delete(id);
+            }
+
+            @GetMapping("/{id}")
+            public Director getById(@PathVariable Long id) {
+                log.info("Запрос режиссёра по id={}", id);
+                return directorService.getById(id);
+            }
+
+            @GetMapping
+            public List<Director> getAll() {
+                log.info("Запрос списка всех режиссёров");
+                return directorService.getAll();
+            }
         }
-        Director updated = directorService.update(director);
-        log.info("Обновлён режиссёр: id={}, name={}", updated.getId(), updated.getName());
-        return updated;
->>>>>>> develop
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-<<<<<<< HEAD
-        log.info("Запрос на удаление режиссёра с id={}", id);
-        directorService.delete(id);
-    }
-
-    @GetMapping("/{id}")
-    public Director getById(@PathVariable Long id) {
-        log.info("Запрос режиссёра по id={}", id);
-        return directorService.getById(id);
-    }
-
-    @GetMapping
-    public List<Director> getAll() {
-        log.info("Запрос списка всех режиссёров");
-        return directorService.getAll();
     }
 }
-=======
-        log.info("Запрос на удаление режиссёра: id={}", id);
-        directorService.delete(id);
-    }
-}
->>>>>>> develop
