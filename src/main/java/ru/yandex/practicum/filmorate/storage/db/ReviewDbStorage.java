c83762b3c4008c1b1539317c75eb09a9e508eaeb
Ilya Naumenko <naumenko951294@yandex.ru>
2026-08-24 15:46:43 +0300

    Функциональность Отзывы. Разрешение конфликтов.

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/ReviewDbStorage.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.storage.ReviewStorage;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
